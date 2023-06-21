4c12be43cd75c2f716303946eb11e1ee78d58f26
Suhas Dissanayake <suhasdissa@protonmail.com>
2023-06-22 00:23:57 +0530

    Fix Bottom Sheet animations

--- app/src/main/java/app/suhasdissa/mellowmusic/ui/components/MainScaffold.kt
+++ app/src/main/java/app/suhasdissa/mellowmusic/ui/components/MainScaffold.kt
@@ -1,21 +1,40 @@
 package app.suhasdissa.mellowmusic.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import app.suhasdissa.mellowmusic.backend.viewmodel.PlayerViewModel
+import app.suhasdissa.mellowmusic.ui.screens.player.FullScreenPlayer
 import app.suhasdissa.mellowmusic.ui.screens.player.MiniPlayer
-import app.suhasdissa.mellowmusic.ui.screens.player.PlayerSheet
 import app.suhasdissa.mellowmusic.utils.mediaItemState
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScaffold(
     fab: @Composable () -> Unit,
@@ -24,8 +43,42 @@ fun MainScaffold(
 ) {
     val playerViewModel: PlayerViewModel = viewModel(factory = PlayerViewModel.Factory)
     var isPlayerSheetVisible by remember { mutableStateOf(false) }
+    val playerSheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = true
+    )
+    val scope = rememberCoroutineScope()
     if (isPlayerSheetVisible) {
-        PlayerSheet(onDismissRequest = { isPlayerSheetVisible = false })
+        ModalBottomSheet(
+            onDismissRequest = { isPlayerSheetVisible = false },
+            sheetState = playerSheetState,
+            shape = RoundedCornerShape(8.dp),
+            dragHandle = null
+        ) {
+            Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+                Row {
+                    IconButton(onClick = {
+                        scope.launch { playerSheetState.hide() }.invokeOnCompletion {
+                            if (!playerSheetState.isVisible) {
+                                isPlayerSheetVisible = false
+                            }
+                        }
+                    }) {
+                        Icon(Icons.Default.ExpandMore, contentDescription = "Close Player")
+                    }
+                    Text(text = "Now Playing", style = MaterialTheme.typography.titleLarge)
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(Icons.Default.MoreVert, "Song Options")
+                }
+            }
+            Divider(Modifier.fillMaxWidth())
+            playerViewModel.controller?.let { controller ->
+                FullScreenPlayer(
+                    controller,
+                    onToggleFavourite = { playerViewModel.toggleFavourite(it) })
+            }
+
+        }
     }
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -36,7 +89,12 @@ fun MainScaffold(
                 val mediaItem by controller.mediaItemState()
                 mediaItem?.let {
                     MiniPlayer(
-                        onClick = { isPlayerSheetVisible = true },
+                        onClick = {
+                            isPlayerSheetVisible = true
+                            scope.launch {
+                                playerSheetState.show()
+                            }
+                        },
                         controller,
                         it,
                         onPlayPause = { playerViewModel.playPause() },
--- app/src/main/java/app/suhasdissa/mellowmusic/ui/screens/songs/SongsScreen.kt
+++ app/src/main/java/app/suhasdissa/mellowmusic/ui/screens/songs/SongsScreen.kt
@@ -1,24 +1,33 @@
 package app.suhasdissa.mellowmusic.ui.screens.songs
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Shuffle
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import app.suhasdissa.mellowmusic.backend.viewmodel.PlayerViewModel
-import app.suhasdissa.mellowmusic.ui.components.MainScaffold
 
 @Composable
 fun SongsScreen(
     showFavourites: Boolean,
     playerViewModel: PlayerViewModel = viewModel(factory = PlayerViewModel.Factory)
 ) {
-    MainScaffold(fab = {
+    Scaffold(floatingActionButton = {
         FloatingActionButton(onClick = { if (showFavourites) playerViewModel.shuffleFavourites() else playerViewModel.shuffleAll() }) {
             Icon(imageVector = Icons.Default.Shuffle, contentDescription = "Shuffle")
         }
-    }) {
-        SongListView(showFavourites)
+    }) { innerPadding ->
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(innerPadding)) {
+            SongListView(showFavourites)
+        }
     }
 }
