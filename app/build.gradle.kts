d0a1b6a6a1efbdd65b79daca5a80b5c28c8ba622
Suhas Dissanayake <suhasdissa@protonmail.com>
2023-07-18 13:12:08 +0530

    Bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "app.suhasdissa.mellowmusic"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
