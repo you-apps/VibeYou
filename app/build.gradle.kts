b65db662d160bab91d73ff0a672efeba01cf23d6
Suhas Dissanayake <suhasdissa@protonmail.com>
2023-07-22 14:15:19 +0530

    Bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "app.suhasdissa.mellowmusic"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -87,30 +87,19 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.0-alpha01")
 
     implementation("androidx.compose.material:material-icons-extended:1.4.3")
-    implementation("androidx.media3:media3-session:1.0.2")
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2022.10.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
-
 
     implementation("io.coil-kt:coil-compose:2.3.0")
 
-
-    val media3_version = "1.0.2"
+    val media3Version = "1.0.2"
 
     // For media playback using ExoPlayer
-    implementation("androidx.media3:media3-exoplayer:$media3_version")
+    implementation("androidx.media3:media3-exoplayer:$media3Version")
     // For HLS playback support with ExoPlayer
-    implementation("androidx.media3:media3-exoplayer-hls:$media3_version")
+    implementation("androidx.media3:media3-exoplayer-hls:$media3Version")
     // For building media playback UIs
-    implementation("androidx.media3:media3-ui:$media3_version")
+    implementation("androidx.media3:media3-ui:$media3Version")
 
-
+    implementation("androidx.media3:media3-session:$media3Version")
 
     val roomVersion = "2.5.1"
 
@@ -123,4 +112,12 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2022.10.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
