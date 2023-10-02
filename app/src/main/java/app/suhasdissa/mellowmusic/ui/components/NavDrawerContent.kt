292853b4ccca1b1e6d5b35de08328669027fc007
Suhas Dissanayake <suhasdissa@gmail.com>
2023-10-02 20:06:41 +0530

    feat: add piped icon

--- app/src/main/java/app/suhasdissa/mellowmusic/ui/components/NavDrawerContent.kt
+++ app/src/main/java/app/suhasdissa/mellowmusic/ui/components/NavDrawerContent.kt
@@ -8,8 +8,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MusicNote
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.rounded.MusicNote
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
@@ -53,7 +53,7 @@ fun NavDrawerContent(
         NavigationDrawerItem(
             icon = {
                 Icon(
-                    imageVector = Icons.Default.MusicNote,
+                    painter = painterResource(id = R.drawable.ic_piped),
                     contentDescription = null
                 )
             },
@@ -68,7 +68,7 @@ fun NavDrawerContent(
         NavigationDrawerItem(
             icon = {
                 Icon(
-                    imageVector = Icons.Default.MusicNote,
+                    imageVector = Icons.Rounded.MusicNote,
                     contentDescription = null
                 )
             },
