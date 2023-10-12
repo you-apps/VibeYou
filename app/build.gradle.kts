ba6b5295ed10ce9c58aad2e491dcd376ddd05084
Suhas Dissanayake <suhasdissa@gmail.com>
2023-10-12 12:36:11 +0530

    refactor: change app version to 1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "app.suhasdissa.vibeyou"
         minSdk = 26
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.8"
+        versionCode = 1
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
