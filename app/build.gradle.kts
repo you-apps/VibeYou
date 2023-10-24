4cb41b8c11ac0d9e9b2381df32713c96ae4b569c
Suhas Dissanayake <suhasdissa@gmail.com>
2023-10-24 22:39:50 +0530

    fix: support android 7 (closes  #66)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "app.suhasdissa.vibeyou"
-        minSdk = 26
+        minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
