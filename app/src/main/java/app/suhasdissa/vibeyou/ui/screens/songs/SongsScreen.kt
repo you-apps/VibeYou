473cc3a83bd3640820606e638fb97469eff0afd1
Suhas Dissanayake <suhasdissa@gmail.com>
2024-01-08 18:47:12 +0530

    fix: shuffle not shuffling

--- app/src/main/java/app/suhasdissa/vibeyou/ui/screens/songs/SongsScreen.kt
+++ app/src/main/java/app/suhasdissa/vibeyou/ui/screens/songs/SongsScreen.kt
@@ -2,9 +2,13 @@ package app.suhasdissa.vibeyou.ui.screens.songs
 
 import android.view.SoundEffectConstants
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.PlayArrow
 import androidx.compose.material.icons.rounded.Shuffle
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -14,6 +18,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import app.suhasdissa.vibeyou.R
 import app.suhasdissa.vibeyou.backend.viewmodel.PlayerViewModel
@@ -32,14 +37,28 @@ fun SongsScreen(
             it.asSong
         }
     Scaffold(floatingActionButton = {
-        FloatingActionButton(onClick = {
-            view.playSoundEffect(SoundEffectConstants.CLICK)
-            playerViewModel.playSongs(songs)
-        }) {
-            Icon(
-                imageVector = Icons.Rounded.Shuffle,
-                contentDescription = stringResource(R.string.shuffle)
-            )
+        Row {
+            FloatingActionButton(onClick = {
+                view.playSoundEffect(SoundEffectConstants.CLICK)
+                playerViewModel.playSongs(songs)
+            }) {
+                Icon(
+                    imageVector = Icons.Rounded.PlayArrow,
+                    contentDescription = stringResource(R.string.play_all)
+                )
+            }
+
+            Spacer(modifier = Modifier.width(12.dp))
+
+            FloatingActionButton(onClick = {
+                view.playSoundEffect(SoundEffectConstants.CLICK)
+                playerViewModel.playSongs(songs, shuffle = true)
+            }) {
+                Icon(
+                    imageVector = Icons.Rounded.Shuffle,
+                    contentDescription = stringResource(R.string.shuffle)
+                )
+            }
         }
     }) { innerPadding ->
         Column(
