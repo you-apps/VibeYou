087650c99e446cfe9c2406f68b4af876ab366bf8
Bnyro <bnyro@tutanota.com>
2024-01-31 18:31:47 +0100

    chore: bump version to v3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "app.suhasdissa.vibeyou"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "3.0"
 
         ksp {
             arg("room.schemaLocation", "$projectDir/schemas")
